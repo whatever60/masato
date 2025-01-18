056291ef24ec64fcd9dae62db8120d3b8cd53330
whatever60 <quym@foxmail.com>
2025-01-18 18:54:41 -0500

    update demo script

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -55,7 +55,7 @@ usearch_workflow.py search_global \
 
 # Command 3: Run workflow_per_sample
 usearch_workflow.py workflow_per_sample \
-  --input_fastq "$data_dir/output_unoise3_16s/merged_isolate.fq.gz" \
+  --input_fastq "$data_dir/output_unoise3_16s/merged_iso.fq.gz" \
   --output_path "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.json" \
   --search
 
@@ -172,3 +172,131 @@ plot_dm.py \
     --distance "braycurtis" \
     --hue "rep_group" \
     --annotate_dots
+
+
+
+# For 16S-ITS isolate sequencing
+isolate_utils.py get_metadata \
+    -i $data_dir/camii_pick \
+    -pm $data_dir/metadata/plates.csv \
+    -o $data_dir/metadata/isolate.tsv
+# Combine bulk and isolate data for 16S
+zcat "$data_dir/output_unoise3_16s/merged_bulk.fq.gz" "$data_dir/output_unoise3_16s/merged_isolate.fq.gz" | pigz > "$data_dir/output_unoise3_16s/merged_iso.fq.gz"
+# Per sample workflow
+usearch_workflow.py workflow_per_sample \
+  --input_fastq "$data_dir/output_unoise3_16s/merged_iso.fq.gz" \
+  --output_path "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.json" \
+  --search
+usearch_workflow.py aggregate_samples \
+  --input_json "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.json" \
+  --output_fasta "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.fa" \
+  --output_count "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.biom" \
+  --prefix "16S-U"
+# Taxonomy and tree
+run_rdp_classifier.py --input "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.fa" --database "16srrna"
+get_tree.py --input_fasta "$data_dir/output_unoise3_16s/unoise3_zotu_isolate.fa"
+
+
+# Trim for ITS bulk and isolate
+trim.py --mode isolate_150 \
+  --input_dir "$data_dir/fastq_isolate" \
+  --output "$data_dir/output_unoise3_its/merged_isolate.fq.gz" \
+  --primer_set "its" \
+  --first_k "110" \
+  --min_length "110" \
+  --barcode_fwd "$barcodes_fwd" \
+  --barcode_rev "$barcodes_rev" \
+  --pattern "$patterns"
+trim.py --mode r1 \
+  --input_dir "$data_dir/fastq_bulk_its" \
+  --output "$data_dir/output_unoise3_its/merged_bulk.fq.gz" \
+  --primer_set "its" \
+  --first_k "110" \
+  --min_length "110"
+# Combine bulk and isolate data for ITS
+zcat "$data_dir/output_unoise3_its/merged_bulk.fq.gz" "$data_dir/output_unoise3_its/merged_isolate.fq.gz" | pigz > "$data_dir/output_unoise3_its/merged_iso.fq.gz"
+# Per sample workflow for ITS
+usearch_workflow.py workflow_per_sample \
+  --input_fastq "$data_dir/output_unoise3_its/merged_iso.fq.gz" \
+  --output_path "$data_dir/output_unoise3_its/unoise3_zotu_isolate.json" \
+  --search
+usearch_workflow.py aggregate_samples \
+  --input_json "$data_dir/output_unoise3_its/unoise3_zotu_isolate.json" \
+  --output_fasta "$data_dir/output_unoise3_its/unoise3_zotu_isolate.fa" \
+  --output_count "$data_dir/output_unoise3_its/unoise3_zotu_isolate.biom" \
+  --prefix "ITS-U"
+# Taxonomy and tree for ITS
+run_rdp_classifier.py --input "$data_dir/output_unoise3_its/unoise3_zotu_isolate.fa" --database "fungalits_unite_v9"
+get_tree.py --input_fasta "$data_dir/output_unoise3_its/unoise3_zotu_isolate.fa"
+
+
+# combine count
+isolate_utils.py combine_count \
+    -m $data_dir/metadata/isolate.tsv \
+    -cb $data_dir/output_unoise3_16s/unoise3_zotu_isolate.biom \
+    -tb $data_dir/output_unoise3_16s/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -cf $data_dir/output_unoise3_its/unoise3_zotu_isolate.biom \
+    -tf $data_dir/output_unoise3_its/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -o $data_dir/interaction
+
+# calculate purity
+isolate_utils.py combine_count \
+    -m $data_dir/metadata/isolate.tsv \
+    -cb $data_dir/output_unoise3_16s/unoise3_zotu_isolate.biom \
+    -tb $data_dir/output_unoise3_16s/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -cf $data_dir/output_unoise3_its/unoise3_zotu_isolate.biom \
+    -tf $data_dir/output_unoise3_its/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -o $data_dir/interaction \
+    -pl otu domain \
+    -cp 0.5
+
+# Bulk-isolate abundance plot for 16S
+threshold_top=5
+plot.py abundance_group heatmap_log10 \
+    -i $data_dir/output_unoise3_16s/unoise3_zotu_isolate.biom \
+    -m $data_dir/metadata/bulk_16s.tsv \
+    -t $data_dir/output_unoise3_16s/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -f $data_dir/figs/abundance_isolate_top${threshold_top}_16s \
+    -a $threshold_top \
+    -l genus \
+    -fo taxonomy_tree \
+    -ii $data_dir/interaction/count_otu_bacteria.biom \
+    -im $data_dir/metadata/isolate.tsv \
+    -ir sample_type \
+    -isp spike_in_16s
+plot.py abundance_group heatmap_raw \
+    -i $data_dir/interaction/count_otu_bacteria.biom \
+    -m $data_dir/metadata/isolate.tsv \
+    -t $data_dir/output_unoise3_16s/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -f $data_dir/figs/isolate_count_16s \
+    -l genus \
+    -a 0.01 \
+    -r plate_name \
+    -s sample_group \
+    -sp spike_in_16s \
+    -fo taxonomy_tree
+# Bulk-isolate abundance plot for ITS
+threshold_top=5
+plot.py abundance_group heatmap_log10 \
+    -i $data_dir/output_unoise3_its/unoise3_zotu_isolate.biom \
+    -m $data_dir/metadata/bulk_its.tsv \
+    -t $data_dir/output_unoise3_its/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -f $data_dir/figs/abundance_isolate_top${threshold_top}_its \
+    -a $threshold_top \
+    -l genus species \
+    -fo taxonomy_tree \
+    -ii $data_dir/interaction/count_otu_fungi.biom \
+    -im $data_dir/metadata/isolate.tsv \
+    -ir sample_type \
+    -isp spike_in_its
+plot.py abundance_group heatmap_raw \
+    -i $data_dir/interaction/count_otu_fungi.biom \
+    -m $data_dir/metadata/isolate.tsv \
+    -t $data_dir/output_unoise3_its/unoise3_zotu_isolate_rrndb_processed.tsv \
+    -f $data_dir/figs/isolate_count_its \
+    -l genus species \
+    -a 0.01 \
+    -r plate_name \
+    -s sample_group \
+    -sp spike_in_its \
+    -fo taxonomy_tree
